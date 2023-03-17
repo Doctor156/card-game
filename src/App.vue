b6f711625ded5f9b1d4b55593da8a60933b9b780
dmitriy.sumatokhin <dmitriy.sumatokhin@sibirix.ru>
2023-03-17 20:26:04 +0700

    * init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <CardsGame msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script lang="ts">
+
+import CardsGame from "./components/CardsGame.vue";
+export default {
+  name: 'App',
+  components: {CardsGame}
+}
+</script>
---
+++ src/components/CardsGame.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card-game__wrapper">
+    Тут будет игра
+  </div>
+</template>
+
+
+<script lang="ts">
+import {Vue} from 'vue-property-decorator';
+
+export default class CardsGame extends Vue {
+  // @Prop({}) came!: ;
+
+  created() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-game {
+  &__wrapper {
+    background-color: red;
+  }
+}
+</style>
